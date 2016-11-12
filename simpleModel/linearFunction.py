7219370e7eb8ddae72c9dfefb5458b293dd79aad
crazyCodeLove <crazyCodeLove@aliyun.com>
2016-11-12 13:37:17 +0800

    add new data

--- simpleModel/linearFunction.py
+++ simpleModel/linearFunction.py
@@ -2,3 +2,15 @@
 
 import tensorflow as tf
 import numpy as np
+
+from matplotlib import pyplot as plt
+
+#create data
+x_train = np.linspace(-1, 1, 1000)[:, np.newaxis]
+y_train = np.square(x_train)
+
+
+#create graph start
+
+
+#create graph end
