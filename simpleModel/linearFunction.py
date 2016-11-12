7e02a37a3d79a12e24c2c0255c48c9c8c38a7cb8
crazyCodeLove <crazyCodeLove@aliyun.com>
2016-11-12 14:26:48 +0800

    add one more hidden layer

--- simpleModel/linearFunction.py
+++ simpleModel/linearFunction.py
@@ -6,7 +6,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 #create data
-x_train = np.linspace(-1, 1, 1000)[:, np.newaxis]
+x_train = np.linspace(-1, 1, 10000)[:, np.newaxis]
 y_train = np.square(x_train)
 
 
@@ -27,19 +27,22 @@ def add_layer(inputs, in_feature_size, out_feature_size, activate_function = Non
 #create graph start
 #set hidden layer 1 features
 inputs_features = 1
-hl1_features = 5
+hl1_features = 10
 xp = tf.placeholder(tf.float32,[None,inputs_features],"inputs")
 
-hidden_layer1 = add_layer(xp, inputs_features, hl1_features, tf.nn.relu)
+hidden_layer1 = add_layer(xp, inputs_features, hl1_features, activate_function = tf.nn.relu)
 
+#set hidden layer 2 features
+# hl2_features = 5
+# hidden_layer2 = add_layer(hidden_layer1, hl1_features, hl2_features, activate_function= tf.nn.relu)
 
 #set outputs features
 outputs_features = 1
 yp = tf.placeholder(tf.float32,[None,outputs_features])
 
 outputs = add_layer(hidden_layer1,hl1_features, outputs_features)
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(outputs - yp), reduction_indices=1))
-train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(outputs - yp), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.initialize_all_variables()
 #create graph end
@@ -49,7 +52,7 @@ sess = tf.Session()
 sess.run(init)
 
 for i in xrange(1000):
-    if i % 200 == 0:
+    if i % 100 == 0:
         print sess.run(loss, feed_dict={xp : x_train, yp : y_train})
 
     sess.run(train_step, feed_dict={xp : x_train, yp : y_train})
