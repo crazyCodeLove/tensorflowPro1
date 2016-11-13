921504d54fbdba1cc8388203ca8c3336fa9c323a
crazyCodeLove <crazyCodeLove@aliyun.com>
2016-11-13 22:59:08 +0800

    use regularization function

--- simpleModel/classModen.py
+++ simpleModel/classModen.py
@@ -29,7 +29,7 @@ inputs_features = 784
 outputs_features = 10
 
 xp = tf.placeholder(tf.float32, [None, inputs_features])
-outputs,Weights = add_layer(xp, inputs_features, outputs_features, "outputs", activate_function= tf.nn.softmax)
+outputs,Weights = add_layer(xp, inputs_features, outputs_features, "outputs")
 
 yp = tf.placeholder(tf.float32,[None, outputs_features])
 #define loss function
@@ -37,8 +37,11 @@ yp = tf.placeholder(tf.float32,[None, outputs_features])
 # train_step = tf.train.GradientDescentOptimizer(0.2).minimize(cross_entropy)
 
     #use regularization to loss
-loss = tf.reduce_mean(-tf.reduce_sum(yp * tf.log(outputs),reduction_indices=[1])) + 0.01 * tf.nn.l2_loss(Weights)
+# loss = tf.reduce_mean(-tf.reduce_sum(yp * tf.log(outputs),reduction_indices=[1])) + 0.01 * tf.nn.l2_loss(Weights)
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(outputs, yp) + 0.01 * tf.nn.l2_loss(Weights))
+
 train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+
 
 # loss = tf.reduce_mean(tf.reduce_sum(tf.square(yp-outputs),reduction_indices=1))
 # train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
@@ -51,7 +54,7 @@ try:
     sess.run(init)
     for i in xrange(10001):
         train_x, train_y = mnist.train.next_batch(120)
-        sess.run(train_step, feed_dict={ xp : train_x, yp : train_y})
+        sess.run([train_step], feed_dict={ xp : train_x, yp : train_y})
         if i % 50 == 0:
             test_x, test_y = mnist.test.next_batch(2000)
             test_result = sess.run(outputs, feed_dict={ xp : test_x})
